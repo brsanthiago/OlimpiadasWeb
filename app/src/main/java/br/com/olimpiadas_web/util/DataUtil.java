ad511da96fb8c1a0dd9de1bc4fb817a7eb1aabfd
Bruno Santiago <bruno.oliveira@gsw.com.br>
2016-08-01 22:49:28 -0300

    Ajuste layout

--- app/src/main/java/br/com/olimpiadas_web/util/DataUtil.java
+++ app/src/main/java/br/com/olimpiadas_web/util/DataUtil.java
@@ -13,7 +13,7 @@ public class DataUtil {
     static String formatLocal = "dd/MM/yyyy HH:mm:ss";
     private static String date;
 
-    public static String getDate(String pubDate){
+    public static String getDate(String pubDate) {
         Date date = new Date();
         try {
             date = getDateObj(pubDate);
@@ -24,12 +24,12 @@ public class DataUtil {
     }
 
     private static Date getDateObj(String pubDate) throws ParseException {
-        SimpleDateFormat pubDateFormat = new SimpleDateFormat(formatRfc2822, Locale.ENGLISH); //rss spec
+        SimpleDateFormat pubDateFormat = new SimpleDateFormat(formatRfc2822, Locale.ENGLISH);
         Date date = null;
         try {
             date = pubDateFormat.parse(pubDate);
         } catch (ParseException e) {
-            pubDateFormat = new SimpleDateFormat(formatLocal); //fallback
+            pubDateFormat = new SimpleDateFormat(formatLocal);
             date = pubDateFormat.parse(pubDate);
         }
         return date;
